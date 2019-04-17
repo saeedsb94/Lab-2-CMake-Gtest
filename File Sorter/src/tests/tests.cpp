446b3d23f3ba11d200f7ac575add9b3e9123e027
Saeed <saeed.sb94@gmail.com>
2019-04-17 22:42:30 +0300

    bla bla

--- File Sorter/src/tests/tests.cpp
+++ File Sorter/src/tests/tests.cpp
@@ -5,34 +5,34 @@
 /*	Case 1:	Sorted sequence	*/
 TEST(SortingTest, sortedInput) {
 
-	EXPECT_TRUE(TestFile("testCase1.txt"));
+	EXPECT_TRUE(TestFile("docs/testCase1.txt"));
 }
 
 /*	Case 2:	Reversed sequence	*/
 TEST(SortingTest, reversedInput) {
 
-	EXPECT_TRUE(TestFile("testCase2.txt"));
+	EXPECT_TRUE(TestFile("docs/testCase2.txt"));
 }
 
 /*	Case 3:	Identical sequence	*/
 TEST(SortingTest, identicalInput) {
 
-	EXPECT_TRUE(TestFile("testCase3.txt"));
+	EXPECT_TRUE(TestFile("docs/testCase3.txt"));
 }
 
 /*	Case 4: Sequence with negative numbers	*/
 TEST(SortingTest, negativeInput) {
 
-	EXPECT_TRUE(TestFile("testCase4.txt"));
+	EXPECT_TRUE(TestFile("docs/testCase4.txt"));
 }
 
 /*	Case 0:	Random sequence	*/
 
 TEST(SortingTest, RandomInput) {
 
-	CreatDataFile("testCase5.txt");
+	CreatDataFile("docs/testCase5.txt");
 
-	EXPECT_TRUE(TestFile("testCase5.txt"));
+	EXPECT_TRUE(TestFile("docs/testCase5.txt"));
 }
 
 
